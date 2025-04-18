f68f7036ab76218cc3786df8d93bc2256465309b
emma188 <54875722+emma188@users.noreply.github.com>
2025-04-18 15:33:56 -0400

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -3,4 +3,4 @@ yum install -y docker || amazon-linux-extras install docker -y || true
 systemctl start docker || service docker start || true
 usermod -aG docker ec2-user || true
 sleep 10
-docker run -d -p 8080:8080 emma188/hello_springboot
+docker run -d -p 8080:8080 emma188/hello-springboot
